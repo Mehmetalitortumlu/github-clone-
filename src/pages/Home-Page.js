7d3ccdf0fc170a19b326a92c7f19e87c7f39d0c3
Mehmetalitortumlu <mehmetalitortumlu@gmail.com>
2023-03-08 21:10:27 +0300

    alert ad..

--- src/pages/Home-Page.js
+++ src/pages/Home-Page.js
@@ -6,10 +6,16 @@ import Navbar from '../components/Navbar'
 
 function Home() {
     return (
-            <div className='bg-light min-vh-100' >
-                <Navbar />
-                <Main />
+        <div className='bg-light min-vh-100' >
+            <div className='alert alert-primary alert-dismissible' role={"alert"}>
+                <strong>
+                    En sağdaki img fotoğrafına tıklayıp daha sonra your repositories tıklayıp new butonuna basarak yeni repositories eklenebiliyor.
+                </strong>
+                <button type='button' className='btn-close' data-bs-dismiss="alert" aria-label='kapat' ></button>
             </div>
+            <Navbar />
+            <Main />
+        </div>
     )
 }
 
